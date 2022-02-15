1371121a853ab47f9402fb7ba6c54b63b3d7566d
cavidrezzaqzade <cavidrazzagzade@gmail.com>
2022-02-15 17:40:13 +0400

    second commit

--- mia-grps-admin-hr-api/src/main/java/az/gov/mia/grp/model/MedalDTO.java
+++ mia-grps-admin-hr-api/src/main/java/az/gov/mia/grp/model/MedalDTO.java
@@ -3,13 +3,10 @@ package az.gov.mia.grp.model;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
-import javax.validation.constraints.NotBlank;
-
 @AllArgsConstructor
 @Data
 public class MedalDTO {
     private Long id;
 
-    @NotBlank(message = "Medal adı boş ola bilməz")
     private String name;
 }
